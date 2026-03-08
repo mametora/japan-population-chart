9592c7482a372cbe2b05a5d108811858568b85a3
Takanori Masuda <mametora@mmtr.me>
2026-03-08 12:43:55 +0900

    Improve theming by adjusting background and shadow styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ export default async function Home() {
   const prefectures = await getPrefectures();
 
   return (
-    <div className="font-sans">
-      <header className="mx-auto max-w-6xl px-4 py-6 sm:px-8">
-        <Typography variant="h1">Japan Population Chart</Typography>
+    <div className="font-sans min-h-dvh bg-gray-100 dark:bg-gray-900">
+      <header className="w-full bg-white dark:bg-gray-800 shadow-md shadow-black/10 dark:shadow-white/5 mb-8">
+        <div className="mx-auto max-w-6xl px-4 py-6 sm:px-8">
+          <Typography variant="h1">Japan Population Chart</Typography>
+        </div>
       </header>
       <main className="mx-auto max-w-6xl w-full px-4 sm:px-8">
         <PrefecturePopulation prefectures={prefectures} />
